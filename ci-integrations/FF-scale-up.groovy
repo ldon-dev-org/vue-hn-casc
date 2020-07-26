37804485d3e532533012fc7d6b9d53214b38e402
Grigory Avsyuk <gavsyuk@Grigorys-MacBook-Pro.local>
2020-07-26 18:29:45 -0400

    Move to ci-integrations folder

---
+++ ci-integrations/FF-scale-up.groovy
@@ -0,0 +1,41 @@
+@Library('cb-days@master') _
+def testPodYaml = libraryResource 'podtemplates/vuejs/vuejs-test-pod.yml'
+pipeline {
+  agent none
+  options { 
+    buildDiscarder(logRotator(numToKeepStr: '10'))
+    //skipDefaultCheckout true
+    preserveStashes(buildCount: 10)
+  }
+  environment {
+    //Possible canary deploy phases:
+    //1. Flag completely off
+    //2. Flag turned on for internal testing
+    //3. Flag turned on for internal testing + % subset of end users
+    canaryPhases = ["killSwitch", "fullyFalse", "internalTesting", "percentageDeploy"]
+    currentPhase = "fullyFalse"
+    testsPass = false
+  }
+  stages('FF Scale Up')
+  {
+    stage('Pre-scale'){
+       agent {
+        kubernetes {
+          label 'nodejs'
+          yaml testPodYaml
+        }
+      }
+      steps {
+        checkout scm
+        script{
+
+        }
+      }
+    }
+    stage('Scale Up'){
+      steps {
+        testsPass = automatedTestCall()
+      }
+    }
+  }
+}
